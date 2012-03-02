f9e1b534155035aa807d5e72d3a46829c38fe0cf
Patrik Hedman <patrik.hedman@gmail.com>
2012-03-02 09:38:17 +0100

    added type resolver to the application object

--- spec/serenity/application_spec.js
+++ spec/serenity/application_spec.js
@@ -8,5 +8,25 @@ describe ("Serenity", function () {
       
       expect (App.Foo). toEqual ("Foo");
     });
+    
+    describe ("._resolve_types", function () {
+      it ("can resolve simple types", function () {
+        var App = Serenity.app();
+        App.Foo = Serenity.Object.clone(); App.Bar = Serenity.Object.clone(), App.Bar.Baz = Serenity.Object.clone();
+        App._resolve_types();
+        
+        expect ( App.Foo.type     ). toEqual ("Foo");
+        expect ( App.Bar.type     ). toEqual ("Bar");
+      });
+      
+      it ("can resolve nested types", function () {
+        var App = Serenity.app();
+        App.Foo = Serenity.Object.clone(); App.Foo.Bar = Serenity.Object.clone(), App.Foo.Bar.Baz = Serenity.Object.clone();
+        App._resolve_types();
+        
+        expect ( App.Foo.Bar.type     ). toEqual ("Bar");
+        expect ( App.Foo.Bar.Baz.type ). toEqual ("Baz");
+      });
+    });
   });
 });
