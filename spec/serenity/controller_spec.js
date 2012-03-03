f38760a7c50829dc34e2f9c713548f33b6487485
Patrik Hedman <patrik.hedman@gmail.com>
2012-03-03 13:23:04 +0100

    added Serenity.Controller and a very simple spec to verify that it loads properly

---
+++ spec/serenity/controller_spec.js
@@ -0,0 +1,8 @@
+
+describe ("Serenity", function () {
+  describe ("Controller", function () {
+    it ("has a type field", function () {
+      expect (Serenity.Controller.type). toEqual ("Serenity.Controller");
+    });
+  });
+});
