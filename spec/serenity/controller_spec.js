d548337bdb81e3748170b88eb5db4d042c0a49e3
Patrik Hedman <patrik.hedman@gmail.com>
2012-03-05 10:28:52 +0100

    added create method to controllers

--- spec/serenity/controller_spec.js
+++ spec/serenity/controller_spec.js
@@ -4,5 +4,19 @@ describe ("Serenity", function () {
     it ("has a type field", function () {
       expect (Serenity.Controller.type). toEqual ("Serenity.Controller");
     });
+    
+    describe (".create", function () {
+      it ("delegates to the model's create method", function () {
+        var App = Serenity.app();
+        App.Task  = { create: function () {} };
+        App.Tasks = Serenity.ArrayController.clone();
+        App.run();
+        spyOn(App.Task, 'create');
+        
+        App.Tasks.create("foo");
+        
+        expect (App.Task.create). toHaveBeenCalledWith("foo");
+      });
+    });
   });
 });
