91f90711f5cabcfa3ea8f8cc78a7f66fc640fe6f
Patrik Hedman <patrik.hedman@gmail.com>
2012-03-05 10:34:56 +0100

    added Serenity.Model and a simple spec to verify it

---
+++ spec/serenity/model_spec.js
@@ -0,0 +1,8 @@
+
+describe ("Serenity", function () {
+  describe ("Model", function () {
+    it ("has a type field", function () {
+      expect (Serenity.Model.type). toEqual ("Serenity.Model");
+    });
+  });
+});
