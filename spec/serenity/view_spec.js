e9ab684d9c28d1b5dc6d24f31c8193cc15d0ffa6
Patrik Hedman <patrik.hedman@gmail.com>
2012-03-05 19:05:52 +0100

    added Serenity.View

---
+++ spec/serenity/view_spec.js
@@ -0,0 +1,8 @@
+
+describe ("Serenity", function () {
+  describe ("View", function () {
+    it ("it has a type", function () {
+      expect (Serenity.View.type). toEqual ("Serenity.View");
+    });
+  });
+});
