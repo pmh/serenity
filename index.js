28f452871fc513a35b90fca8449fc0fd9d10b6ac
Patrik Hedman <patrik.hedman@gmail.com>
2012-03-08 23:44:52 +0100

    added enumerable module and an each method

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/serenity')
+module.exports = require('./lib/serenity');
--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -6,3 +6,4 @@ var Empty = function () {}, Identity = function (x) { return x; };
 global.ko = { observable: Identity, observableArray: Identity, applyBindings: Identity, computed: Identity };
 global.$Context = { html: Identity, find: function () { return this;}, each: function () {}, append: function () {}, fadeIn: function () {}, fadeOut: function () {} };
 global.$  = function (selector) { return global.$Context; };
+global._  = {};
