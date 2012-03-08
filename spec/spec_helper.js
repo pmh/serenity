9255de096e082ba3a260625f70f99e7f07f52828
Patrik Hedman <patrik.hedman@gmail.com>
2012-03-08 22:53:53 +0100

    added renderTemplate method

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -4,5 +4,5 @@ global.Serenity = require('../lib/serenity');
 // Mock Objects
 var Empty = function () {}, Identity = function (x) { return x; };
 global.ko = { observable: Identity, observableArray: Identity, applyBindings: Identity, computed: Identity };
-global.$Context = { html: Identity };
+global.$Context = { html: Identity, find: function () { return this;}, each: function () {}, append: function () {}, fadeIn: function () {}, fadeOut: function () {} };
 global.$  = function (selector) { return global.$Context; };
